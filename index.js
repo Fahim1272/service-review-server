e556f0dbc124032f7ab3bfa12385b0abca37bc23
Fahim1272 <imfahim72@gmail.com>
2022-11-09 11:54:09 +0600

    mongoDb connected

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-const packageName = require('express');
+const express = require('express');
 const cors = require('cors');
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 
 // Middlewares----------------------------------!
 app.use(cors());
 app.use (express.json())
+
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+
+//Mongodb Connection---------------------------!
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.msm1u0w.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/',(req, res)=>{
     res.send('Genius Car Server is Running')
