65d8f6423bad4f9abe7e2594461fc09d091fbf00
Fahim1272 <imfahim72@gmail.com>
2022-11-10 13:05:19 +0600

    added dynamic route for service details

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 
 // Middlewares----------------------------------!
 app.use(cors());
-app.use (express.json())
-
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASSWORD);
+app.use(express.json())
 
 //Mongodb Connection---------------------------!
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.msm1u0w.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+    try {
+        const serviceCollection = client.db('photography').collection('services');
+        app.get('/services', async(req,res) =>{
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+        app.get('/services/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)}
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+    } finally {
+
+    }
+}
+run().catch(err => console.error(err))
 
 
-app.get('/',(req, res)=>{
-    res.send('Genius Car Server is Running')
+app.get('/', (req, res) => {
+    res.send('Photography server is Running')
 })
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`server Running on ${port}`);
 })
